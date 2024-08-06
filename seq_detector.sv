88d22be4b6ab1da454a9a73f1fa45a910cf628d4
Shashank V M <Shashank V M>
2024-08-06 20:29:32 +0530

    Move to EBMC

--- seq_detector.sv
+++ seq_detector.sv
@@ -13,7 +13,7 @@ module seq_detector(
      else       state <= next;
 
    // Next state assignment logic
-   always_comb begin: set_next_state
+   always_comb begin
      next = state;
      unique case (state)
        S0 : if (seq_in) next = S1; else next = S0;
@@ -22,34 +22,24 @@ module seq_detector(
        S3 : if (seq_in) next = S4; else next = S2;
        S4 : if (seq_in) next = S1; else next = S2;
      endcase
-   end: set_next_state
+   end
 
    // Registered output logic
    always_ff @(posedge clk, posedge reset)
      if (reset) detect_out <= 1'b0;
      else       detect_out <= (state == S4);
 
-`ifdef FORMAL
 
-restrict property (!reset != $initstate);
+ASSUME_VALID_STATE1: assume property ($onehot(state));
+ASSUME_VALID_STATE2: assume property (reset |-> (state == S0));
 
-default clocking @(posedge clk);
-endclocking
+ASSERT_OVERLAPPING_PROPERTY: assert property (@(posedge clk) (state == S4) |-> (next != S0));
+ASSERT_CHK_SEQ_DETECT: assert property (@(posedge clk) disable iff (reset) seq_in ##1 !seq_in ##1 seq_in ##1 seq_in  |-> ##2 detect_out);
+ASSERT_BI: assert property ( @(posedge clk)  disable iff (reset) detect_out |-> (($past(seq_in, 2) == 1) && ($past(seq_in, 3) == 1) && ($past(seq_in, 4) == 0) && ($past(seq_in, 5) == 1)) );
 
-default disable iff (reset);
+COVER_NON_OVERLAPPING_SEQUENCE: cover property (seq_in ##1 !seq_in ##1 seq_in ##1 seq_in ##2 !seq_in);
+COVER_OVERLAPPING_SEQUENCE: cover property (seq_in ##1 !seq_in ##1 seq_in ##1 seq_in ##1 !seq_in ##1 seq_in ##1 seq_in ##3 seq_in);
 
-sequence SEQ;
-  seq_in ##1 !seq_in ##1 seq_in[*2];
-endsequence
 
-property CHK_SEQ_DETECT;
-   SEQ |=> ##1 detect_out;
-endproperty;
-
-ASSERT_CHK_SEQ_DETECT: assert property (CHK_SEQ_DETECT);
-ASSERT_ONE_HOT_STATE_ENCODING: assert property ($onehot(state));
-SEQ_DETECT_WITNESS: cover property (SEQ ##2 detect_out);
-
-`endif
 
 endmodule
